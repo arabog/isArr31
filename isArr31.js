62f3403653a74e79b1ae45b07e76e9a435bf0849
iCode_Rx <arababs2015@gmail.com>
2021-07-15 14:29:42 +0100

    3rd soln

--- isArr31.js
+++ isArr31.js
@@ -40,6 +40,13 @@ function blackAndWhite(arr){
                               ? 'black' : 'white' : 'fake'} array`;
 }
 
+// anoda soln
+const blackAndWhite = arr =>
+  `It's a ${!Array.isArray(arr) 
+          ? `fake` 
+          : arr.includes(5) && arr.includes(13) 
+          ? `black` : `white`} array`;
+
 console.log(blackAndWhite(5,13) );
 
 console.log(blackAndWhite([5,13]));
